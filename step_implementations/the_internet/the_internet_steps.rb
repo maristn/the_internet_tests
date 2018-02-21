55c28ba4e994a814c62ff85446a5d7b3d46b3434
Mariana <marianasantana@gmail.com>
2018-02-20 22:57:31 -0300

    update

--- step_implementations/the_internet/the_internet_steps.rb
+++ step_implementations/the_internet/the_internet_steps.rb
@@ -47,39 +47,3 @@ step 'Check if the table is ordered alphabetically' do
     puts "Last names aren't ordered alphabetically"
   end
 end
-
-
-
-
-
-
-# require_relative '../spec_helper.rb'
-#
-
-#
-# step 'Type "text" on search field' do |text|
-#   main_page.fill_field('input',text)
-#   @search_text = text
-# end
-#
-#
-# step 'The results page must be shown' do
-#   main_page.save_screenshot("reports/images/result_page.png", full:true)
-#   if main_page.title != "#{@search_text} - Google Search"
-#     raise "Result page was not shown"
-#   end
-# end
-#
-# step 'The current page or the results page must not be shown' do
-#   main_page.save_screenshot("reports/images/no_result_page.png", full:true)
-#   if main_page.title == "#{@search_text} - Google Search" || main_page.title == "Google"
-#     raise "Result or current page was shown"
-#   end
-# end
-#
-# step 'The results page must show the text "results_text"' do |results_text|
-#   main_page.save_screenshot("reports/images/#{results_text}.png", full:true)
-#   if main_page.get_message != results_text
-#     raise "#{results_text} was not shown"
-#   end
-# end
